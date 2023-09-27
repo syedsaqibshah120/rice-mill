835b2dfeb8cd754f6965a527fb9af0434a563ef6
asjedshah164 <95270214+asjedshah164@users.noreply.github.com>
2023-09-27 22:25:53 +0500

    add seed file on backend

---
+++ WahidRiceTradingAPI/Context/SeedData.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using WahidRiceTradingAPI.Context;
+using WahidRiceTradingAPI.Models;
+namespace WahidRiceTradingAPI.Context
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>());
+
+            // Check if there are any existing users
+            if (context.Users.Any())
+            {
+                return; // Database has been seeded
+            }
+
+            // Create an administrator user
+            var adminUser = new User
+            {
+                FirstName = "Admin",
+                LastName = "User",
+                UserName = "admin",
+                Password = "admin123" // You should hash the password in a real application
+            };
+
+            context.Users.Add(adminUser);
+            context.SaveChanges();
+        }
+    }
+}
--- WahidRiceTradingAPI/Program.cs
+++ WahidRiceTradingAPI/Program.cs
@@ -1,10 +1,10 @@
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using WahidRiceTradingAPI.Context;
 using WahidRiceTradingAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -17,9 +17,23 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
+
+// Seed the database with administrator user
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        SeedData.Initialize(services);
+    }
+    catch (Exception ex)
+    {
+        // Log any exceptions if needed
+        Console.WriteLine(ex.Message);
+    }
+}
 
 app.Run();
